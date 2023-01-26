139b77331fbce3534afe29cda0a28c747f4f1fde
OIOpyH3e <99492609+OIOpyH3e@users.noreply.github.com>
2023-01-26 21:53:00 +0600

    Removed pages links into module

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,4 @@
-const fortune = require('./lib/fortune');
+const handlers = require('./lib/handlers');
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 
@@ -16,24 +16,15 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', handlers.home);
 
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() });
-});
+app.get('/about', handlers.about);
 
 //Пользовательская страница 404
-app.use((req, res) => {
-    res.status(404);
-    res.render('404');
-});
+app.use(handlers.notFound);
 
 //Пользовательская страница 500
-app.use((err, req, res, next) => {
-    console.error(err.message);
-    res.status(500);
-    res.render('500');
-});
+app.use(handlers.serverError);
 
 app.listen(port, () =>
     console.log(
