78729f837f061c179f42ad8f0b9520c11bd64b19
OIOpyH3e <99492609+OIOpyH3e@users.noreply.github.com>
2023-02-01 22:24:20 +0600

    Добавлены утилиты для тестирования 'portfinder' и 'puppeteer'

--- meadowlark.js
+++ meadowlark.js
@@ -26,9 +26,13 @@ app.use(handlers.notFound);
 //Пользовательская страница 500
 app.use(handlers.serverError);
 
-app.listen(port, () =>
-    console.log(
-        `Express запущен на http://localhost:${port}; ` +
-            `нажмите CTRL+C для завершения.`
-    )
-);
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(
+            `Express запущен на http://localhost:${port}` +
+                `; нажмите Ctrl+C для завершения.`
+        );
+    });
+} else {
+    module.exports = app;
+}
