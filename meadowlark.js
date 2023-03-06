1a9de11622f8e5553c95df4b47133f3c071d45b1
OIOpyH3e <99492609+OIOpyH3e@users.noreply.github.com>
2023-03-06 18:18:34 +0600

    Добавлен хелпер и исправлены файлы.

--- meadowlark.js
+++ meadowlark.js
@@ -16,6 +16,13 @@ app.engine(
     'handlebars',
     expressHandlebars.engine({
         defaultLayout: 'main',
+        helpers: {
+            section: function (name, options) {
+                if (!this._sections) this._sections = {};
+                this._sections[name] = options.fn(this);
+                return null;
+            },
+        },
     })
 );
 app.set('view engine', 'handlebars');
