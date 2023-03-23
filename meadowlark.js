81be0d8227a425a536aea1e19f2a7e9c2b31ca76
OIOpyH3e <99492609+OIOpyH3e@users.noreply.github.com>
2023-03-23 06:07:34 +0600

    Добавлена функциональность загрузки фотографий на сервер.

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
 
 const handlers = require('./lib/handlers');
 const weatherMiddleware = require('./lib/middleware/weather');
@@ -49,6 +50,19 @@ app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
 
 app.get('/newsletter', handlers.newsletter);
 app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
+
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest);
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+    const form = new multiparty.Form();
+    form.parse(req, (err, fields, files) => {
+        if (err) return res.status(500).send({ error: err.message });
+        handlers.vacationPhotoContestProcess(req, res, fields, files);
+    });
+});
+app.get(
+    '/contest/vacation-photo-thank-you',
+    handlers.vacationPhotoContestProcessThankYou
+);
 
 //Пользовательская страница 404
 app.use(handlers.notFound);
